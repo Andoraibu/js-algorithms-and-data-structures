c1fe8f18a967416a8e58c214ef4e0c6ff97d5ec6
andoraibu <elbroandrew@gmail.com>
2021-07-26 00:47:17 +1000

    перенес в папку recursion

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import bubbleSort from './bubbleSort.js'
-import sumToOne from './sumToOne.js'
+import sumToOne from './recursion/sumToOne.js'
 
 let x = bubbleSort([23, 54, 65, -12, 0, 5, 87, 331, 6]);
 console.log(x);
