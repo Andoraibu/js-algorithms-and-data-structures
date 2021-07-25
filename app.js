ae3aea448b8f68a6f91612e7edcb32fe8133e041
andoraibu <elbroandrew@gmail.com>
2021-07-26 00:36:41 +1000

    вынес bubbleSort в отдельный модуль

--- app.js
+++ app.js
@@ -1,30 +1,4 @@
-function bubbleSort(array)
-{
-    for(let i = array.length; i > 0; i -= 1)
-    {
-        let noSwaps = true; //optimization, to not check all numbers that has already been checked
-
-        for(let j = 0; j < i - 1; j += 1)
-        {
-
-            if(array[j] > array[j+1])
-            {
-                //SWAP
-                let temp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = temp;
-                noSwaps = false;
-            }
-
-            
-        }
-
-        if(noSwaps) break;
-
-    }
-
-    return array;
-}
+import bubbleSort from './bubbleSort.js'
 
 let x = bubbleSort([23, 54, 65, -12, 0, 5, 87, 331, 6]);
 console.log(x);
---
+++ bubbleSort.js
@@ -0,0 +1,29 @@
+function bubbleSort(array)
+{
+    for(let i = array.length; i > 0; i -= 1)
+    {
+        let noSwaps = true; //optimization, to not check all numbers that has already been checked
+
+        for(let j = 0; j < i - 1; j += 1)
+        {
+
+            if(array[j] > array[j+1])
+            {
+                //SWAP
+                let temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+                noSwaps = false;
+            }
+
+            
+        }
+
+        if(noSwaps) break;
+
+    }
+
+    return array;
+}
+
+export default bubbleSort;
