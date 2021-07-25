56d54f6e90f0822da0619ae71bc8969288c28d90
andoraibu <elbroandrew@gmail.com>
2021-07-26 00:53:58 +1000

    факториал

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 import bubbleSort from './bubbleSort.js'
 import sumToOne from './recursion/sumToOne.js'
+import factorial from './recursion/factorial.js'
 
-let x = bubbleSort([23, 54, 65, -12, 0, 5, 87, 331, 6]);
-console.log(x);
+//раскомментировать необходимый пример
+
+//let x = bubbleSort([23, 54, 65, -12, 0, 5, 87, 331, 6]);
+//console.log(x);
 
 
-let sum1 = sumToOne(5);
-console.log(sum1);
+//let sum1 = sumToOne(5);
+//console.log(sum1);
+
+let fact = factorial(5);
+console.log(fact);
