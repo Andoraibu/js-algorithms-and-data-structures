10bd4d45f4da3da88f7b5b7b60a2d06bb8da477d
andrew <elbroandrew@gmail.com>
2021-10-18 00:43:57 +1100

    feat: add is prime solution better solution

--- is_prime/app.js
+++ is_prime/app.js
@@ -11,4 +11,23 @@ function isPrime_1(n){
     return true;
 }
 
-console.log(isPrime_1(4));
+// console.log(isPrime_1(4));
+// console.log(isPrime_1(5));
+
+//ряд вверху кроме 2 и 3 паттерн считается как 6*k +- 1, т.е.
+//6*1-1 = 5, 6*1+1 = 7, 6*2-1 = 11, 6*2+1=13 и тд.
+function isPrime2(n){ //O(sqrt(n)) - much faster.
+    if(n <= 1) return false;
+    if(n <= 3) return true;
+    //пропускаем следующие 5 номеров
+    if(n % 2 == 0 || n % 3 == 0) return false;
+
+    for(let i = 5; i*i < n; i+=6){
+        if(n % i == 0 || n % (i + 2) == 0){
+            return false;
+        }
+
+        return true;
+    }
+
+}
