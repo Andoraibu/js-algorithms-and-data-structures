34151e6ef620936fbeb08189b409472dff2f273c
elbroandrew <elbroandrew@gmail.com>
2022-11-21 10:38:44 +1000

    add ladder chaining

--- calc_object.js
+++ calc_object.js
@@ -14,6 +14,24 @@ let calculator = {
         }
     }
 }
+//
+// calculator.read(10, 12);
+// console.log(calculator.sum());
 
-calculator.read(10, 12);
-console.log(calculator.sum());
+/* chaining */
+let ladder = {
+    step: 0,
+    up() {
+        this.step++;
+        return this;
+    },
+    down() {
+        this.step--;
+        return this;
+    },
+    showStep: function() { // показывает текущую ступеньку
+        console.log( this.step );
+    }
+};
+
+ladder.up().up().down().showStep();
